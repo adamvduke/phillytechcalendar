6a32f347cc2fd0ed961304b5f33f0695e1fbaa91
Aaron Kuehler <AaronKuehler@gmail.com>
2011-05-27 23:18:08 -0400

    Removed old url building logic.

--- calendar_app.rb
+++ calendar_app.rb
@@ -11,40 +11,8 @@ class CalendarApp < Sinatra::Base
   set :public, File.dirname(__FILE__) + '/public'
   
   get '/' do
-    ids = %w(phillytechcalendar@gmail.com, h70l6uq02gllc4k9innk8qu28c@group.calendar.google.com) 
+    ids = %w(phillytechcalendar@gmail.com h70l6uq02gllc4k9innk8qu28c@group.calendar.google.com) 
     @src = build(ids)
-
-    # up to @style these need to be concatenated to form
-    # the url for the calendar using the & character
-    # src attribute for the iframe
-    @src = "https://www.google.com/calendar/embed?"
-
-    # show the print link or not
-    @src = @src + "showPrint=0&"
-
-    # show the drop down that lists the aggregated calendars or not
-    @src = @src + "showCalendars=0&"
-
-    # height of the calendar
-    @src = @src + "height=600&"
-
-    # the start day of the week Sunday=1 Monday=2 Saturday=7
-    @src = @src + "wkst=1&"
-
-    # background color for the calendar
-    @src = @src + "bgcolor=%23c0c0c0&"
-
-    # could potentially build up a larger list of calendars here
-    # phillytechcalendar
-    @src = @src + "src=phillytechcalendar%40gmail.com&"
-    @src = @src + "color=%23182C57&"
-
-    # some shared calendar
-    @src = @src + "src=h70l6uq02gllc4k9innk8qu28c%40group.calendar.google.com&"
-    @src = @src + "color=%23856508&"
-
-    # time zone
-    @src = @src + "ctz=America%2FNew_York"
 
     # other attributes for the iframe
     @style = "border-width:0"
--- lib/calendar_url_helper.rb
+++ lib/calendar_url_helper.rb
@@ -5,34 +5,34 @@ module Sinatra
       # up to @style these need to be concatenated to form
       # the url for the calendar using the & character
       # src attribute for the iframe
-      @src = "https://www.google.com/calendar/embed?"
+      url = "https://www.google.com/calendar/embed?"
 
       # show the print link or not
-      @src << "showPrint=" << (options[:show_print] || "0") << "&"
+      url << "showPrint=" << (options[:show_print] || "0") << "&"
 
       # show the drop down that lists the aggregated calendars or not
-      @src << "showCalendars=" << (options[:show_calendars] || "0") << "&"
+      url << "showCalendars=" << (options[:show_calendars] || "0") << "&"
 
       # height of the calendar
-      @src << "height=" << (options[:height] || "600") << "&"
+      url << "height=" << (options[:height] || "600") << "&"
 
       # the start day of the week Sunday=1 Monday=2 Saturday=7
-      @src << "wkst=" << (options[:week_start] || "1") << "&"
+      url << "wkst=" << (options[:week_start] || "1") << "&"
 
       # background color for the calendar
-      @src << "bgcolor=%23" << (options[:bg_color] || "c0c0c0") << "&"
+      url << "bgcolor=%23" << (options[:bg_color] || "c0c0c0") << "&"
 
       # could potentially build up a larger list of calendars here
       # phillytechcalendar
       calendar_ids.each do | calendar_id |
-        @src << "src=#{CGI::escape(calendar_id)}" << "&"
-        @src << "color=%23" << (options[:event_color] || "182C57") << "&"
+        url << "src=#{CGI::escape(calendar_id)}" << "&"
+        url << "color=%23" << (options[:event_color] || "182C57") << "&"
       end
       
       # time zone
       time_zone = options[:time_zone]
       time_zone = CGI::escape(time_zone) if time_zone
-      @src << "ctz=" << (time_zone || "America%2FNew_York")
+      url << "ctz=" << (time_zone || "America%2FNew_York")
     end
   end  
 end
