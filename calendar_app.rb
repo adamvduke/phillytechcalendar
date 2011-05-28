4b4c4570c6303d36ebc0641a0a5bc272ad585e64
Aaron Kuehler <AaronKuehler@gmail.com>
2011-05-27 23:05:23 -0400

    Added calendar URL helper.

--- calendar_app.rb
+++ calendar_app.rb
@@ -1,14 +1,18 @@
 require 'rubygems' if RUBY_VERSION < "1.9"
 require 'sinatra/base'
 require 'partial_helper'
+require 'calendar_url_helper'
 
 class CalendarApp < Sinatra::Base
   helpers Sinatra::PartialHelper
+  helpers Sinatra::CalendarUrlHelper
   
   set :static, true
   set :public, File.dirname(__FILE__) + '/public'
   
   get '/' do
+    ids = %w(phillytechcalendar@gmail.com, h70l6uq02gllc4k9innk8qu28c@group.calendar.google.com) 
+    @src = build(ids)
 
     # up to @style these need to be concatenated to form
     # the url for the calendar using the & character
