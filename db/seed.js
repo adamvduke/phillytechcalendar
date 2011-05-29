9f97e8d06bf87117a8fc0214593e2f4a06e8575a
Aaron Kuehler <aaronkuehler@gmail.com>
2011-05-29 10:03:23 -0400

    Added other seed calendar.

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,2 @@
-db.calendars.insert({id:'bv1dmjq3fd16ks12042ejoa0ug@group.calendar.google.com', name:'Philly Events'});
+db.calendars.insert({id:'bv1dmjq3fd16ks12042ejoa0ug@group.calendar.google.com', name:'Philly Group1'});
+db.calendars.insert({id:'n72tsjpj1gkr0vn4udnvg80k7o@group.calendar.google.com', name:'Philly Group2'});
