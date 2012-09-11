249902d19d9d7157bfc1bb28379d9d7cae6d59c1
Adam Duke <adam.v.duke@gmail.com>
2012-09-11 18:11:04 -0400

    fix up js formatting

--- public/js/application.js
+++ public/js/application.js
@@ -1,24 +1,24 @@
 function calendar_ids_for_selector(selector) {
 	var ids_array = [];
-    $(selector).each(function() {
-        ids_array.push($(this).val());
-    });
-    return ids_array;
+	$(selector).each(function() {
+		ids_array.push($(this).val());
+	});
+	return ids_array;
 }
 
 function checked_calendar_ids() {
-    return calendar_ids_for_selector(".shared-calendars :checked");
+	return calendar_ids_for_selector(".shared-calendars :checked");
 }
 
 function unchecked_calendar_ids() {
-    return calendar_ids_for_selector(".shared-calendars input:checkbox:not(:checked)");
+	return calendar_ids_for_selector(".shared-calendars input:checkbox:not(:checked)");
 }
 
 var eventSources = {};
 
 function refresh_calendars(calendars) {
 	var unchecked = unchecked_calendar_ids();
-	$.each(unchecked, function(index, id){
+	$.each(unchecked, function(index, id) {
 		var eventSource = eventSources[id];
 		if (eventSource != null) {
 			$('.calendar').fullCalendar('removeEventSource', eventSource.url);
@@ -26,35 +26,35 @@ function refresh_calendars(calendars) {
 		};
 	});
 
-    $.each(calendars, function(index, calendar) {
-	    var eventSource = eventSources[calendar.cal_id];
+	$.each(calendars, function(index, calendar) {
+		var eventSource = eventSources[calendar.cal_id];
 		if (eventSource === undefined || eventSource === null) {
 			$('.calendar').fullCalendar('addEventSource', calendar);
-	        eventSources[calendar.cal_id] = calendar;
+			eventSources[calendar.cal_id] = calendar;
 		}
-    });
+	});
 }
 
 function get_checked_calendars() {
-    $.ajax({
-        url: "/calendars",
-        type: "GET",
-        data: {
-            ids: checked_calendar_ids()
-        },
-        success: function(data) {
-            refresh_calendars(data);
-        },
-        error: function(data) {
-            console.log("ERROR: " + data);
-        }
-    })
+	$.ajax({
+		url: "/calendars",
+		type: "GET",
+		data: {
+			ids: checked_calendar_ids()
+		},
+		success: function(data) {
+			refresh_calendars(data);
+		},
+		error: function(data) {
+			console.log("ERROR: " + data);
+		}
+	})
 }
 
 $(document).ready(function() {
-    $('.calendar').fullCalendar({
-	theme: true,
-	weekMode: 'liquid'
-});
-    get_checked_calendars();
+	$('.calendar').fullCalendar({
+		theme: true,
+		weekMode: 'liquid'
+	});
+	get_checked_calendars();
 })
