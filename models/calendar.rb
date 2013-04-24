39fb158ed0f0806254ce949e0d3bcf56cd20956d
Adam Duke <adam.v.duke@gmail.com>
2013-04-24 01:17:20 -0400

    slightly refactor to_json

--- models/calendar.rb
+++ models/calendar.rb
@@ -1,6 +1,6 @@
 class Calendar
   include MongoMapper::Document
-  
+
   key :cal_id, String, :required => true
   key :name, String, :require => true
   key :color, :default => '182C57'
@@ -8,9 +8,12 @@ class Calendar
   Calendar.ensure_index [[:cal_id, 1]], :unique => true
 
   def to_json(arg1, arg2)
-    url = %Q|"https://www.google.com/calendar/feeds/#{self.cal_id}%40group.calendar.google.com/public/basic"|
-    color = self.color
-    json = %Q|{"cal_id": "#{cal_id}", "url": #{url}, "color": "##{color}", "textColor": "black"}|
+    json = %Q|{"cal_id": "#{cal_id}", "color": "##{color}", "name": "#{name}", "textColor": "black", "url": #{calendar_url}}|
     json.to_s
   end
+
+  private
+    def calendar_url
+      %Q|"https://www.google.com/calendar/feeds/#{cal_id}%40group.calendar.google.com/public/basic"|
+    end
 end
