074126a24a4e30f3702a3d336185d8fc02770623
Adam Duke <adam.v.duke@gmail.com>
2013-04-24 01:14:08 -0400

    update the way the index is created

--- models/calendar.rb
+++ models/calendar.rb
@@ -1,9 +1,11 @@
 class Calendar
   include MongoMapper::Document
   
-  key :cal_id, String, :required => true, :index => true
+  key :cal_id, String, :required => true
   key :name, String, :require => true
   key :color, :default => '182C57'
+
+  Calendar.ensure_index [[:cal_id, 1]], :unique => true
 
   def to_json(arg1, arg2)
     url = %Q|"https://www.google.com/calendar/feeds/#{self.cal_id}%40group.calendar.google.com/public/basic"|
